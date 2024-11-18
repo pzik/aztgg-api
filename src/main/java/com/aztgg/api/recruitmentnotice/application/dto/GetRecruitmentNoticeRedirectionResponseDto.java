7f85bdea77f31b98ea707d153b6be3634cd53689
suhongkim98 <kshz9815@naver.com>
2024-11-18 21:47:29 +0900

    feat: 공고 응답에 id 식별자 필드 추가

--- src/main/java/com/aztgg/api/recruitmentnotice/application/dto/GetRecruitmentNoticeRedirectionResponseDto.java
+++ src/main/java/com/aztgg/api/recruitmentnotice/application/dto/GetRecruitmentNoticeRedirectionResponseDto.java
@@ -5,13 +5,15 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record GetRecruitmentNoticeRedirectionResponseDto(String jobOfferTitle,
-                                      String companyCode,
-                                      @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") LocalDateTime startAt,
-                                      @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") LocalDateTime endAt) {
+public record GetRecruitmentNoticeRedirectionResponseDto(Long recruitmentNoticeId,
+                                                         String jobOfferTitle,
+                                                         String companyCode,
+                                                         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") LocalDateTime startAt,
+                                                         @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'") LocalDateTime endAt) {
 
     public static GetRecruitmentNoticeRedirectionResponseDto from(RecruitmentNotice recruitmentNotice) {
-        return new GetRecruitmentNoticeRedirectionResponseDto(recruitmentNotice.getJobOfferTitle(),
+        return new GetRecruitmentNoticeRedirectionResponseDto(recruitmentNotice.getRecruitmentNoticeId(),
+                recruitmentNotice.getJobOfferTitle(),
                 recruitmentNotice.getCompanyCode(),
                 recruitmentNotice.getStartAt(),
                 recruitmentNotice.getEndAt());
