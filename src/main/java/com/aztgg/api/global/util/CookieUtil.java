15bdb4b49bd09bd573f699aea1bf712e3597abaa
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2025-06-29 17:32:49 +0900

    refactor: cookieUtil validation (#10)

--- src/main/java/com/aztgg/api/global/util/CookieUtil.java
+++ src/main/java/com/aztgg/api/global/util/CookieUtil.java
@@ -1,14 +1,16 @@
 package com.aztgg.api.global.util;
 
-import org.springframework.stereotype.Component;
-
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 @Component
 public class CookieUtil {
 
-
     public void setCookie(HttpServletResponse response, String name, String value, int maxAge, boolean httpOnly, boolean secure) {
+        validateCookieName(name);
+        validateCookieValue(value);
+
         StringBuilder cookieBuilder = new StringBuilder();
         cookieBuilder.append(name).append("=").append(value);
         cookieBuilder.append("; Max-Age=").append(maxAge);
@@ -28,6 +30,19 @@ public class CookieUtil {
     }
 
     public void removeCookie(HttpServletResponse response, String name) {
+        validateCookieName(name);
         response.setHeader("Set-Cookie", name + "=; Max-Age=0; Path=/; HttpOnly; Secure; SameSite=Strict");
     }
-} 
+
+    private static void validateCookieName(String name) {
+        if (!StringUtils.hasLength(name) || name.length() < 1) {
+            throw new IllegalArgumentException("Cookie name must be at least 1 character");
+        }
+    }
+
+    private static void validateCookieValue(String value) {
+        if (!StringUtils.hasLength(value) || value.length() < 1) {
+            throw new IllegalArgumentException("Cookie value must be at least 1 character");
+        }
+    }
+}
